9cc90ef7b2dd22670fe15d6c3db8934b38ade8bf
Alena Chaschevaia <alena.sarycheva.99@bk.ru>
2025-06-18 02:31:00 +0300

    homework done

--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,24 @@
 import json
 import os
 
-from src.product import Product
 from src.category import Category
+from src.product import Product
+
 
 def read_json(path: str) -> dict:
     full_path = os.path.abspath(path)
-    with open(full_path, 'r', encoding="UTF-8") as file:
+    with open(full_path, "r", encoding="UTF-8") as file:
         data = json.load(file)
     return data
+
 
 def create_objects_from_json(data):
     categories = []
     for category in data:
         products = []
-        for product in category['products']:
+        for product in category["products"]:
             products.append(Product(**product))
-        category['products'] = products
+        category["products"] = products
         categories.append(Category(**category))
 
     return categories

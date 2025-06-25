b91e2d187fb103d11a879e1337dd72076f9c5b04
Alena Chaschevaia <alena.sarycheva.99@bk.ru>
2025-06-25 23:28:32 +0300

    homework done

--- src/product.py
+++ src/product.py
@@ -80,8 +80,26 @@ class Product:
                 self.__price = new_price
                 print(f"Цена изменена на {new_price}")
 
+
 class ZeroQuantityProduct(Exception):
     """Класс ошибки при нулевом количестве товара"""
 
     def __init__(self, message: Optional[str] = None):
         super().__init__(message)
+
+    class PrintMixin:
+        """ "Класс-миксин, который при создании объекта печатает в консоль информацию о том,
+        от какого класса и с какими параметрами был создан объект"""
+
+        name: str
+        description: str
+        price: float
+        quantity: int
+
+        def __init__(self) -> None:
+            print(repr(self))
+
+        def __repr__(self) -> str:
+            """Метод, который реализует вывод информации в консоль согласно заданному шаблону:
+            *Product('Продукт1', 'Описание продукта', 1200, 10)*"""
+            return f"{self.__class__.__name__} ({self.name}, {self.description}, {self.price}, {self.quantity})"

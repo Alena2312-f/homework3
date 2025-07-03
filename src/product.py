78aaacdc9c9ea5a7d6187f0516b1416bbaf60672
Alena Chaschevaia <alena.sarycheva.99@bk.ru>
2025-07-04 00:51:35 +0300

    homework 16_2 done

--- src/product.py
+++ src/product.py
@@ -1,7 +1,10 @@
 from typing import Any, List, Optional
 
+from src.base_product import BaseProduct
+from src.print_mixin import PrintMixin
 
-class Product:
+
+class Product(BaseProduct, PrintMixin):
     name: str
     description: str
     price: float
@@ -86,21 +89,3 @@ class ZeroQuantityProduct(Exception):
 
     def __init__(self, message: Optional[str] = None):
         super().__init__(message)
-
-
-class PrintMixin:
-    """ "Класс-миксин, который при создании объекта печатает в консоль информацию о том,
-    от какого класса и с какими параметрами был создан объект"""
-
-    name: str
-    description: str
-    price: float
-    quantity: int
-
-    def __init__(self) -> None:
-        print(repr(self))
-
-    def __repr__(self) -> str:
-        """Метод, который реализует вывод информации в консоль согласно заданному шаблону:
-        *Product('Продукт1', 'Описание продукта', 1200, 10)*"""
-        return f"{self.__class__.__name__} ({self.name}, {self.description}, {self.price}, {self.quantity})"
